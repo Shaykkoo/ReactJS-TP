66ac1797e746981ac69e1a1708189430888fab64
Micheline <micheline.xavir@u-paris.fr>
2024-08-01 14:49:57 +0200

    Try

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import { Route, Routes, Navigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import Home from './pages/Home';
+import Register from './pages/Register';
+
 
 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/Login';
+
 
 function App() {
 
@@ -14,7 +18,10 @@ function App() {
     <Box>
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      
+      </Routes> 
     </Box>
   )
 }
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Typography, Box, List, ListItem, ListItemText, Paper, AppBar, Toolbar, Button } from '@mui/material';
 
-function Home() {
+const Home = () => {
+  const user = JSON.parse(localStorage.getItem('user')) || { name: 'Utilisateur' };
+
   return (
-    <div>
-        <p>Home</p>
-    </div>
-  )
-}
+    <>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Bienvenue, {user.name}!
+          </Typography>
+          <Button color="inherit" component={Link} to="/profile">Profil</Button>
+          <Button color="inherit" component={Link} to="/login">Déconnexion</Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Paper elevation={3} sx={{ padding: 3, mt: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Tableau de Bord
+          </Typography>
+          <List>
+            <ListItem button component={Link} to="/profile">
+              <ListItemText primary="Profil" />
+            </ListItem>
+            <ListItem button component={Link} to="/cabinets">
+              <ListItemText primary="Liste des cabinets" />
+            </ListItem>
+            <ListItem button component={Link} to="/animals">
+              <ListItemText primary="Gestion des animaux" />
+            </ListItem>
+            <ListItem button component={Link} to="/search">
+              <ListItemText primary="Recherche de cabinets" />
+            </ListItem>
+          </List>
+        </Paper>
+      </Container>
+    </>
+  );
+};
 
-export default Home
+export default Home;
